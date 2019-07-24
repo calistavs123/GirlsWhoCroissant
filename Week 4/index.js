76cc369b714aacc15b3e5cdf10caba6b48cedecd
GWC User <calisty702@gmail.com>
2019-07-24 15:49:04 -0400

    hihihh

--- Week 4/index.js
+++ Week 4/index.js
@@ -3,39 +3,39 @@ var i = 0;
 for (I = 0; I <= 20; I += 2){
   console.log(i);
 }
+//
+// i = 0; //sets i to 0
+// while(i <= 20){    //runs when i<= 20
+//   console.log(i);
+//   i += 2;
+// }
+//
+// i = 0;
+// while(i <= 20){
+//   if(i % 2 == 0){
+//     console.log(i);
+//   }
+//   i += 1;
+// }
+// function gettemp(){
+//   return 22.5;
+// }
+//
+// var temperature = getTemp();
+//
+//
+//
+// console.log(temperature);
+//
+// function getTemp2(type){
+//   if (type == "c"){
+//     return 22.5;
+//   }
+//   if (type == "f"){
+//     alert("it is really hot today!")
+//     return 100;
+//   }
+// }
 
-i = 0; //sets i to 0
-while(i <= 20){    //runs when i<= 20
-  console.log(i);
-  i += 2;
-}
-
-i = 0;
-while(i <= 20){
-  if(i % 2 == 0){
-    console.log(i);
-  }
-  i += 1;
-}
-function gettemp(){
-  return 22.5;
-}
-
-var temperature = getTemp();
-
-
-
-console.log(temperature);
-
-function getTemp2(type){
-  if (type == "c"){
-    return 22.5;
-  }
-  if (type == "f"){
-    alert("it is really hot today!")
-    return 100;
-  }
-}
-
-console.log(getTemp2("f"))
-console.log(getTemp2("c"))
+// console.log(getTemp2("f"))
+// console.log(getTemp2("c"))
